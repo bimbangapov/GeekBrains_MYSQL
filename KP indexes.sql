7123c1662b7ef95655981ab7cd88615a92a3c552
Bimba Angapov <bimbangapov@yandex.ru>
2021-02-03 21:13:32 +0300

    Создание индексов курсового проекта

---
+++ KP indexes.sql
@@ -0,0 +1,9 @@
+create index actors_first_name_last_name  on actors(first_name, last_name);
+
+create index name_of_film on films(name_of_film);
+
+create index head_of_news on news(head);
+
+create index login_of_user on users(login);
+
+create index name_of_user on users(name);
